3a4f524c7aeb694a82cde90fde9b427542e92469
Eric Rucker <bhtooefr@bhtooefr.org>
2014-09-19 20:13:33 -0400

    Update ffstp.js

--- ffstp.js
+++ ffstp.js
@@ -1,2 +1,8 @@
 function loadTracks(filename) {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        // time to parse shit
+    }
+    else {
+        alert('Your browser does not support the File API, and is not compatible with FFSTP.');
+    }
 }
